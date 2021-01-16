0bea35a7e931d9b8bd0230f6b33b238d76b40513
jomapormentilla <jomapormentilla@gmail.com>
2021-01-15 20:31:38 -0500

    Generate resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :departments
+  resources :stores
+  resources :ratings
+  resources :reviews
+  resources :comments
+  resources :products
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
