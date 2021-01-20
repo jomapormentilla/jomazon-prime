1ba03586e2f1a016fd44268e6686a5f333f4550c
jomapormentilla <jomapormentilla@gmail.com>
2021-01-19 21:42:31 -0500

    Add field_with_errors styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    validates :password, confirmation: true
     validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :password, confirmation: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
     validates :company_name, presence: true
 
     scope :is_a_seller, -> { where(account_type: 2) }
