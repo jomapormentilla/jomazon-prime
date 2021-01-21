0328e31bb21e4286e8ea0f3b220c82f600e52740
jomapormentilla <jomapormentilla@gmail.com>
2021-01-21 18:02:30 -0500

    Include Users in search results. Refine SQL queries for rendering products in multiple controller routes.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,14 @@ class ProductsController < ApplicationController
         if params[:user_id]
             @user = User.find_by_id(params[:user_id])
             @sellers = User.is_a_seller.order(:company_name)
-            @products = @user.products.includes(:seller).order(:name)
+            @products = @user.products.includes(:seller, :ratings).order(:name)
 
         elsif params[:department_id]
             @department = Department.find_by_id(params[:department_id])
-            @products = @department.products.includes(:seller).order(:name)
+            @products = @department.products.includes(:seller, :ratings).order(:name)
 
         else
-            @products = Product.all.includes(:seller).order(:name)
+            @products = Product.all.includes(:seller, :ratings).order(:name)
         end
     end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     before_action :redirect_if_signup_incomplete
 
     def home
-        @departments = Department.all.includes(products: [:seller])
+        @departments = Department.all.includes(products: [:seller, :ratings])
         @sellers = User.all.where(account_type: 2).order(:company_name)
     end
 
@@ -12,6 +12,9 @@ class WelcomeController < ApplicationController
             redirect_to root_path
         end
 
-        @products = Product.where("name LIKE ?", "%#{ params[:q] }%")
+        @products = Product.where("name LIKE ?", "%#{ params[:q] }%").includes(:ratings, :seller, :department)
+        @users = User.where("first_name LIKE ?", "%#{ params[:q] }%")
+                    .or(User.where("last_name LIKE ?", "%#{ params[:q] }%"))
+                    .or(User.where("company_name LIKE ?", "%#{ params[:q] }%"))
     end
 end
